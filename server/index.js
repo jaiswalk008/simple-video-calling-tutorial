5f35463729b03aa542b4f2c628961e4b1be73d65
Karan Jaiswal <jaiskaran008@gmail.com>
2023-12-31 03:30:27 +0530

    added peer to peer video call initial connection

--- server/index.js
+++ server/index.js
@@ -6,12 +6,28 @@ const io = new Server(8000 , {
 
 const emailToSocketIdMap = new Map();
 const socketIdToEmailMap = new Map();
+//io refers to socket.io server instance
 io.on('connection' , (socket) =>{
     console.log('socket connected:' +socket.id);
+    /*socket.on('room:join', data => {...}): This sets up a listener for the 'room:join'
+     event on the socket object. When a client sends a 'room:join' event,
+      the provided callback function is executed, and it receives the data object 
+      sent by the client. */
     socket.on('room:join',data =>{
         const  {email , room} = data;
         emailToSocketIdMap.set(email , socket.id);
         socketIdToEmailMap.set(socket.id,email);
+        //it will send  the data to same client who joined using the socket
+        io.to(room).emit('user:joined', {email, id:socket.id});
+        socket.join(room);
         io.to(socket.id).emit('room:join',data)
+    })
+    socket.on('user:call',({to,offer}) =>{
+        
+        io.to(to).emit('incomming:call' , {from:socket.id , offer});
+    })
+
+    socket.on('call:accepted', ({to , ans}) =>{
+        io.to(to).emit('call:accepted' , {from:socket.id , ans});
     })
 })
